20e6b519c89950b74c15e8fb107be310b3161f54
3111084 <3111084@TE-GDA-D-D12C25>
2022-03-25 12:03:17 +0100

    Dodaj pliki projektów.

---
+++ cezar/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace cezar
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj fraze");
+            foreach (var c in Console.ReadLine())
+            {
+                Console.Write(Convert.ToChar((c - 'a' + 3) % ('z' - 'a' + 3) + 'a'));
+            }
+            Console.WriteLine();
+            Console.ReadKey(true);
+        }
+    }
+}
