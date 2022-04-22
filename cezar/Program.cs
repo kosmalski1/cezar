81b78a97797c8a4f46af6f35d89053bdc6f9ed19
kosmalski1 <100939915+kosmalski1@users.noreply.github.com>
2022-04-22 12:24:46 +0200

    Update Program.cs

--- cezar/Program.cs
+++ cezar/Program.cs
@@ -1,7 +1,4 @@
 // See https://aka.ms/new-console-template for more information
-
-
-
 namespace cezar
 
 {
@@ -12,29 +9,33 @@ namespace cezar
             bool aa;
             Console.WriteLine("podaj klucz przesunięcia");
             int przesuniecie = Int32.Parse(Console.ReadLine());
-Console.WriteLine("Podaj fraze"); 
+            Console.WriteLine("Podaj fraze");
             string fraza = Console.ReadLine();
             char[] tablica = fraza.ToCharArray();
-            for (int i = 0; i < tablica.Length;i++)
+            for (int i = 0; i < tablica.Length; i++)
             {
-                tablica[i] =(char)((char)tablica[i] + przesuniecie);
+                tablica[i] = (char)((char)tablica[i] + przesuniecie);
                 aa = Char.IsUpper(tablica[i]);
-                if(aa == true) { 
-                
-                    
+                if (tablica[i] >= 91)
+                {
+                    tablica[i] = (char)((char)tablica[i] - 26); }
+                if (aa == true)
+                {
+
+
 
                 }
                 else
                 {
- tablica[i] = (char)((char)tablica[i] + przesuniecie -2);
+                    tablica[i] = (char)((char)tablica[i] + przesuniecie - 2);
                 }
 
 
 
-               
 
 
-                
+
+
             }
             string charsStr = new string(tablica);
             Console.WriteLine(charsStr);
